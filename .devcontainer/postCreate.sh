c33750b1d9f20deccb7aa3fd5d1fff57251fca41
Adal Adwaid Vikas <44499017+Advi729@users.noreply.github.com>
2026-09-12 19:51:50 +0530

    Update postCreate.sh
    Alpine uses 'apk add' instead of 'apt-get install'

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install sl
+# sudo apt-get update
+# sudo apt-get install sl
+# Alpine uses 'apk add' instead of 'apt-get install'
+sudo apk update
+sudo apk add sl
+
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
 echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
